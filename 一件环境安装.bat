259280128d75e70def20ebb748c0631d2f70ce5d
datao2233 <122881769+datao2233@users.noreply.github.com>
2023-01-26 14:26:14 +0800

    Add files via upload

---
+++ 一件环境安装.bat
@@ -0,0 +1,9 @@
+title 一键安装脚本
+cd C:\WINDOW\system32
+pip install requests
+pip3 install numpy scipy matplotlib -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install urllib
+pip3 install multiprocess
+pip install json
+pip install numpy
+pip install urllib3==3.8
